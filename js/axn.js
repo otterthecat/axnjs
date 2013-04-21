813c68dfe6601342dcb2f7def1d760a11e5df37a
Otter <otc@otterthecat.com>
2013-04-20 21:49:55 -0500

    Reformatted code in sample.js Refactored & cleaned up some code in axn.js

--- js/axn.js
+++ js/axn.js
@@ -1,6 +1,8 @@
 var axn = (function(){
 
-	var defaults = {
+	var _version = "0.02";
+
+	var _defaults = {
 
 		data_name: "axn",
 		root_selector: null,
@@ -9,7 +11,7 @@ var axn = (function(){
 	};
 
 	// utility to merge object properties
-	var merge = function(orig_obj, new_obj){
+	var _merge = function(orig_obj, new_obj){
 
 		if(typeof orig_obj === 'object' && typeof new_obj === 'object'){
 
@@ -17,25 +19,25 @@ var axn = (function(){
 
 				orig_obj[item] = new_obj[item];
 			}
+
 			return orig_obj;
 		}
 
 		return false;
 	};
 
-
 	// regex to help parse param attribute
 	// TODO - refactor this regex to trim off leading/trailing
-	//        whitespace from param value
-	var param_reg_ex = /:?[a-zA-Z0-9-_.\/\?=\!\&]+/g;
+	// whitespace from param value
+	var _param_regex = /:?[a-zA-Z0-9-_.\/\?=\!\&]+/g;
 
 	// object to contain user defined functions
-	var fn = {};
+	var _fn = {};
 
 	// object to hold actions defined in data attributes
-	var actions = {};
+	var _actions = {};
 
-	var stored_data = {};
+	var _stored_data = {};
 
 	var _bind_target = {};
 
@@ -44,24 +46,24 @@ var axn = (function(){
 
 	// initializer object that's to be called
 	// just before closure returns the 'axn' object
-	var init = function(callback){
+	var _init = function(callback){
 
 		document.onreadystatechange = function(){
 
 			if(document.readyState == 'complete'){
 
-				// get all actions
-				find_actions(defaults.root_selector);
+				// get all _actions
+				_find_actions(_defaults.root_selector);
 
 				callback();
 
 				// fire action functions on page/dom load
-				apply_actions();
+				_apply_actions();
 			}
 		};
 	};
 
-	var execute_fn = function(action, func, binds){
+	var _execute_fn = function(action, func, binds){
 
 		// TODO - better handle checks for JSONP
 		// (I just wanted to get the feature in fast)
@@ -69,20 +71,19 @@ var axn = (function(){
 
 			action.element.addEventListener(action.evt, function(event){
 
-				//event.preventDefault();
-
 				if(!action.jsonp){
 
 					func.call(action.element, action.params);
+
 					if(action.bindings.length > 0){
 
-						update_bindings(action.bindings[0][0].name, action.params);
+						_update_bindings(action.bindings[0][0].name, action.params);
 					}
 				} else{
 
-					do_jsonp(action, func);
+					_do_jsonp(action, func);
 				};
-						
+
 			}, false);
 		} else {
 
@@ -91,55 +92,54 @@ var axn = (function(){
 				func.call(action.element, action.params);
 				if(action.bindings.length > 0){
 
-					update_bindings(action.bindings[0][0].name, action.params);
+					_update_bindings(action.bindings[0][0].name, action.params);
 				}
 			} else {
 
-				do_jsonp(action, func);
+				_do_jsonp(action, func);
 			}
 		}
-
 	};
 
-	var update_bindings = function(namespace, data, live){
+	var _update_bindings = function(namespace, data, live){
 
 			var do_live = typeof live === 'boolean' && live === true;
 
-			var all_actions = actions[namespace];
+			var all_actions = _actions[namespace];
 			for(var i = 0; i < all_actions.length; i += 1){
-			
-				merge(actions[namespace][i].params, data);
 
-				if(typeof actions[namespace][i].jsonp === "string" && do_live){
+				_merge(_actions[namespace][i].params, data);
 
-					do_jsonp(actions[namespace][i], fn[namespace]);
+				if(typeof _actions[namespace][i].jsonp === "string" && do_live){
+
+					_do_jsonp(_actions[namespace][i], _fn[namespace]);
 				} else if(do_live){
 
-					fn[namespace].call(actions[namespace][i].element, actions[namespace][i].params);
+					_fn[namespace].call(_actions[namespace][i].element, _actions[namespace][i].params);
 				}
 			};
 	};
 
-	// waits for DOM to be fully loaded before looping through actions object
-	// to get properties via namespace, and maps it out to the 'fn' object
-	var apply_actions = function(){
+	// waits for DOM to be fully loaded before looping through _actions object
+	// to get properties via namespace, and maps it out to the '_fn' object
+	var _apply_actions = function(){
 
-		for(var item in actions){
+		for(var item in _actions){
 
-			if(fn.hasOwnProperty(item)){
+			if(_fn.hasOwnProperty(item)){
 
-				for(var i = 0; i < actions[item].length; i += 1){
+				for(var i = 0; i < _actions[item].length; i += 1){
 
-					execute_fn(actions[item][i], fn[item], actions[item][i].bindings);
+					_execute_fn(_actions[item][i], _fn[item], _actions[item][i].bindings);
 				}
 			}
 		}
 	};
 
 
-	// grab defined actions from page DOM via
+	// grab defined _actions from page DOM via
 	// the set data attributes in the html/view
-	var find_actions = function(selector){
+	var _find_actions = function(selector){
 
 		var all_elements;
 
@@ -152,47 +152,46 @@ var axn = (function(){
 			all_elements = document.querySelectorAll("*");
 		}
 
-		return parse_data_elements(all_elements);
+		return _parse_data_elements(all_elements);
 	};
 
-	// goes through an array of elements and updates the 'actions' object with
+	// goes through an array of elements and updates the '_actions' object with
 	// arrays of objects containing an individual element and parameter object
-	// for use when the 'fn' object is finally required.
-	var parse_data_elements = function(el_array){
+	// for use when the '_fn' object is finally required.
+	var _parse_data_elements = function(el_array){
 
 		for(var i = 0; i < el_array.length; i += 1){
 
-			if(el_array[i].hasAttribute('data-' + defaults.data_name)){
+			if(el_array[i].hasAttribute('data-' + _defaults.data_name)){
 
 				var el = el_array[i];
-				var el_action = el.getAttribute('data-' + defaults.data_name);
+				var el_action = el.getAttribute('data-' + _defaults.data_name);
 
-				if(!actions.hasOwnProperty(el_action)){
+				if(!_actions.hasOwnProperty(el_action)){
 
-					actions[el_action] = new Array();
+					_actions[el_action] = new Array();
 				};
 
-				actions[el_action].push({
+				_actions[el_action].push({
 					name: el_action,
-					params: parse_params(el),
-					evt: parse_attr(el, 'event'),
+					params: _parse_params(el),
+					evt: _parse_attr(el, 'event'),
 					element: el,
-					jsonp: parse_attr(el, 'jsonp'),
+					jsonp: _parse_attr(el, 'jsonp'),
 					bindings: []
 				});
 			}
 		}
-
 	};
 
 
 	// takes an array of elements and looks for the params data attribute
 	// and then converts the string value to a proper javascript object
-	var parse_params = function(element_obj){
+	var _parse_params = function(element_obj){
 
 		var the_params = {};
 
-		var str_value = element_obj.getAttribute('data-' + defaults.data_name + '-params'); 
+		var str_value = element_obj.getAttribute('data-' + _defaults.data_name + '-params'); 
 
 		if(typeof str_value === "string"){
 
@@ -200,7 +199,7 @@ var axn = (function(){
 
 			for(var i = 0; i < name_value_array.length; i += 1){
 
-				var param_array = name_value_array[i].match(param_reg_ex);
+				var param_array = name_value_array[i].match(_param_regex);
 
 				// take first element of array to use as property name
 				var property_name = param_array.shift();
@@ -211,23 +210,23 @@ var axn = (function(){
 			}
 		}
 
-		stored_data[element_obj.getAttribute('data-' + defaults.data_name)] = the_params;
+		_stored_data[element_obj.getAttribute('data-' + _defaults.data_name)] = the_params;
 		return the_params;
 	};
 
 	// get defined data attribute from an element
-	var parse_attr = function(element_obj, attr){
+	var _parse_attr = function(element_obj, attr){
 
-		var str_value = element_obj.getAttribute('data-' + defaults.data_name + '-' + attr);
+		var str_value = element_obj.getAttribute('data-' + _defaults.data_name + '-' + attr);
 
 		return str_value || null;
 	};
 
 
-	var do_jsonp = function(action, callback){
+	var _do_jsonp = function(action, callback){
 
 		var new_script = document.createElement('script');
-		new_script.src =  action.jsonp + '&' + defaults.jsonp_callback + '=' + action.name;
+		new_script.src =  action.jsonp + '&' + _defaults.jsonp_callback + '=' + action.name;
 
 		window[action.name] = function(data){
 
@@ -242,24 +241,24 @@ var axn = (function(){
 	};
 
 
-	// add a property to the 'fn' object defined by namespace, and
+	// add a property to the '_fn' object defined by namespace, and
 	// give it the value of the 2nd argument (a function)
-	var add_fn = function(namespace, func){
+	var _add_fn = function(namespace, func){
 
-		if(fn.hasOwnProperty(namespace)){
+		if(_fn.hasOwnProperty(namespace)){
 
-			console.error("AXN fn object already has property ["+ namespace +"]");
+			console.error("AXN _fn object already has property ["+ namespace +"]");
 			return false;
 		}
 
-		fn[namespace] = func;
+		_fn[namespace] = func;
 	};
 
-	var add_binding = function(namespace, target){
+	var _add_binding = function(namespace, target){
 
 		for(var i = 0; i < target.length; i += 1){
 
-			actions[namespace][0].bindings.push(target);
+			_actions[namespace][0].bindings.push(target);
 		}
 
 		_bind_target = {};
@@ -269,29 +268,12 @@ var axn = (function(){
 	// attach methods to axn's prototype object
 	axn.prototype = {
 
+		version: _version,
+
 		add: function(namespace, func){
 
-			add_fn(namespace, func);
+			_add_fn(namespace, func);
 			return this;
-		},
-
-		update: function(namespace, data, live){
-			
-			var do_live = typeof live === 'boolean' && live === true;
-
-			var all_actions = actions[namespace];
-			for(var i = 0; i < all_actions.length; i += 1){
-			
-				merge(actions[namespace][i].params, data);
-
-				if(typeof actions[namespace][i].jsonp === "string" && do_live){
-
-					do_jsonp(actions[namespace][i], fn[namespace]);
-				} else if(do_live){
-
-					fn[namespace].call(actions[namespace][i].element, actions[namespace][i].params);
-				}			
-			};
 		},
 
 		bind: function(namespace){
@@ -308,20 +290,20 @@ var axn = (function(){
 
 		to: function(namespace, func){
 
-			add_fn(namespace, func);
-			add_binding(namespace, _bind_target);
+			_add_fn(namespace, func);
+			_add_binding(namespace, _bind_target);
 		},
 
 		ready: function(callback){
 
-			init(callback);
+			_init(callback);
 		},
 
 		configure: function(options_obj){
 
 			if(typeof options_obj === 'object'){
-				
-				merge(defaults, options_obj);
+
+				_merge(_defaults, options_obj);
 			} else {
 
 				console.error("AXN.config() requires object literal");
@@ -335,32 +317,29 @@ var axn = (function(){
 		// under certain situations. 
 		getActions: function(property_str){
 
-			return actions.hasOwnProperty(property_str) ? actions[property_str] : actions;
+			return _actions.hasOwnProperty(property_str) ? _actions[property_str] : _actions;
 		},
 
 		getFunctions: function(name_str){
 
-			return fn.hasOwnProperty(name_str) ? fn[name_str] : fn;
+			return _fn.hasOwnProperty(name_str) ? _fn[name_str] : _fn;
 		},
 
 		getStoredData: function(name_str){
 
-			return stored_data.hasOwnProperty(name_str) ? stored_data[name_str] : stored_data;
+			return _stored_data.hasOwnProperty(name_str) ? _stored_data[name_str] : _stored_data;
 		},
 
 		getConfig: function(){
 
-			return defaults;
+			return _defaults;
 		},
 
 		exec: function(){
 
-			apply_actions();
+			_apply_actions();
 		}
 	};
-
-	// initialize to set actions/params
-	//init();
 
 	// return new axn
 	return new axn();
--- js/sample.js
+++ js/sample.js
@@ -2,41 +2,40 @@
 // sample usages
 axn.ready(function(){
 
-// action 'something' is set for 2 different
-// elements in index.html - each with different events
-axn.add('something', function(args){
+	// action 'something' is set for 2 different
+	// elements in index.html - each with different events
+	axn.add('something', function(args){
 
-	this.innerHTML = args.content;
-});
+		this.innerHTML = args.content;
+	});
 
-// action 'otherthing' will be uploaded immmediately
-// after dom is loaded
-axn.add('otherthing', function(args){
+	// action 'otherthing' will be uploaded immmediately
+	// after dom is loaded
+	axn.add('otherthing', function(args){
 
-	this.innerHTML = args.x;
-});
+		this.innerHTML = args.x;
+	});
 
-// action 'something-editor' is a text field
-// that will update paramters for the actions
-// bound to 'something' above
-axn.bind('something').to('something-editor', function(args){
+	// action 'something-editor' is a text field
+	// that will update paramters for the actions
+	// bound to 'something' above
+	axn.bind('something').to('something-editor', function(args){
 
-	// update data params for elements bound
-	//  to the 'something' action
-	args.content = this.value;
-});
+		// update data params for elements bound
+		//  to the 'something' action
+		args.content = this.value;
+	});
 
-// action 'jtothep' will, after the defined event,
-// make a JSONP call, with the callback defined below
-axn.add('jtothep', function(data){
+	// action 'jtothep' will, after the defined event,
+	// make a JSONP call, with the callback defined below
+	axn.add('jtothep', function(data){
 
-	var tweets = "";
-	for(var i = 0; i < data.length; i += 1){
+		var tweets = "";
+		for(var i = 0; i < data.length; i += 1){
 
-		tweets += "<li>" + data[i].text + "</li>";
-	}
-	
-	this.innerHTML += tweets;
-});
+			tweets += "<li>" + data[i].text + "</li>";
+		}
 
+		this.innerHTML += tweets;
+	});
 });
